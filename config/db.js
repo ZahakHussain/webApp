fb6d530d5dd14da6a012b517a3b83033bcfa93c7
ZahakHussain <zahakhussan@gmail.com>
2024-01-03 01:43:05 +0500

    mongoose callback error debugged

--- config/db.js
+++ config/db.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/docker-node-mongo";
 
-const connectDb = () => {
-  mongoose.connect(url, () => {
-    console.log("Connected to MongoDB");
-  });
+const connectDb = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(url);
+    console.log("Connected to MongoDB Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+  // mongoose.connect(url, () => {
+  //   console.log("Connected to MongoDB");
+  // });
 };
 
 module.exports = connectDb;
